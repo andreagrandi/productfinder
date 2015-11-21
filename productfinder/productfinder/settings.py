355c67433d56f3fdd5593bcce59684623c4d4d17
Andrea Grandi <a.grandi@gmail.com>
2015-11-21 23:23:52 +0000

    Add missing STATIC_ROOT folder

--- productfinder/productfinder/settings.py
+++ productfinder/productfinder/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_PATH, "static"),
